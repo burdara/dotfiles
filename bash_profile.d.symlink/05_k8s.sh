a2d6f0a3e285da097a773ed312711cbc24745ef0
Robbie Burda <robbie@cloud-elements.com>
2019-08-06 12:00:05 -0600

    chore: updated k8s config env var:

--- bash_profile.d.symlink/05_k8s.sh
+++ bash_profile.d.symlink/05_k8s.sh
@@ -2,6 +2,8 @@
 #
 # Kubernetes configurations
 
+export KUBECONFIG_HOME="$HOME/.kube"
+
 # Install Kubernetes PS1 scripts.
 # Globals:
 #   None
@@ -33,16 +35,15 @@ _install_k8s_ps1() {
 # Returns:
 #   None
 _source_kube_configs() {
-  KUBECONFIG="$HOME/.kube/config"
-  [[ ! -r "$KUBECONFIG" ]] && touch "$KUBECONFIG"
   local kubecfgs
-  # shellcheck disable=2038
-  kubecfgs="$(grep -R '^kind: Config$' "$HOME"/.kube/* | awk -F: '{print $1}' | grep -v "$HOME/.kube/config" | xargs | tr ' ' ':')"
+  KUBECONFIG="$KUBECONFIG_HOME/config"
+  [[ ! -r "$KUBECONFIG" ]] && touch "$KUBECONFIG"
+  kubecfgs="$(grep '^kind: Config$' "$KUBECONFIG_HOME"/* 2>/dev/null | awk -F: '{print $1}' | grep -v "$KUBECONFIG_HOME/config" | xargs | tr ' ' ':')"
   [[ -n "$kubecfgs" ]] && KUBECONFIG+=":$kubecfgs"
   export KUBECONFIG
 }
 
-[[ ! -d "$HOME/.kube" ]] && mkdir -p "$HOME/.kube"
+[[ ! -d "$KUBECONFIG_HOME" ]] && mkdir -p "$KUBECONFIG_HOME"
 _install_k8s_ps1 && unset _install_k8s_ps1
 _source_kube_configs && unset _source_kube_configs
 export KUBE_EDITOR="vim -u 'NONE'"
