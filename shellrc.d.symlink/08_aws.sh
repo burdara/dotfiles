af8ec451c6212a30ca9a6bab08a592669adf35b2
Robbie Burda <robbie@cloud-elements.com>
2022-05-31 14:23:42 -0600

    fix: source awsp output

--- shellrc.d.symlink/08_aws.sh
+++ shellrc.d.symlink/08_aws.sh
@@ -2,3 +2,10 @@
 #
 # AWS CLI configurations.
 export AWS_SDK_LOAD_CONFIG=1
+
+export AWSP_SELF_OVERRIDE="awsp"
+awsp() {
+  ! hash awsph && echo >&2 "awsph script not found" && return 1
+  awsph $@
+  eval "$(awsph --set)"
+}
