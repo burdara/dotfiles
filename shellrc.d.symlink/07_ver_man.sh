2e0b9d99b34ca04979fd07ef61756bf6e8032f44
Robbie Burda <robbie.burda@uipath.com>
2022-11-14 08:16:14 -0700

    feat: commiting updates 2022-11

--- shellrc.d.symlink/07_ver_man.sh
+++ shellrc.d.symlink/07_ver_man.sh
@@ -12,3 +12,15 @@ command -v nodenv >/dev/null 2>&1 && eval "$(nodenv init -)"
 
 # Python environment
 command -v pyenv >/dev/null 2>&1 && eval "$(pyenv init --path)"
+
+# java versions
+! test -d "$HOME/.jenv/bin" && mkdir -p "$HOME/.jenv/bin"
+add_paths "$HOME/.jenv/bin"
+command -v jenv >/dev/null 2>&1 && eval "$(jenv init -)"
+for jhome in $(find /Library/Java/JavaVirtualMachines -type d -name "Home"); do
+  jenv add "$jhome" >/dev/null
+done
+
+# Version manager init processes don't manage paths well
+# Just in case this runs several times, we will remove deplicates from PATH
+dedupe_path
